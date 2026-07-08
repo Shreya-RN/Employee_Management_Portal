fc24139482a2406b3aebb5e7b6dd0a42d6bcf1fd
Shreya Ravikumar Nayak <198369990+Shreya-RN@users.noreply.github.com>
2026-07-08 11:17:49 +0530

    Update hardcoded password in hashPassword.js

--- Backend/hashPassword.js
+++ Backend/hashPassword.js
@@ -1,6 +1,6 @@
 const bcrypt=require("bcrypt");
 async function generateHash(){
-    const password="admin123";
+    const password="YOUR_PASSWORD";
     const hash=await bcrypt.hash(password,10);
     console.log(hash);
 }
