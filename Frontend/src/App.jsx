04e1f35b626a7552e02cce4c6537ede485ebcd9c
Shreya Ravikumar Nayak <198369990+Shreya-RN@users.noreply.github.com>
2026-07-10 10:08:42 +0530

    update app component

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,11 @@
 import { Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
+import Employees from "./pages/Employees"
 import "./App.css";
+import EmployeeDetails from "./pages/EmployeeDetails";
+import AddEmployee from "./pages/AddEmployee";
+import EditEmployee from "./pages/EditEmployee";
 
 function App() {
   return (
@@ -9,6 +13,10 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/Dashboard" element={<Dashboard />} />
+        <Route path="/employees" element={<Employees />} />
+        <Route path="/employees/:employeeId" element={<EmployeeDetails />} />
+        <Route path="/employees/add" element={<AddEmployee />} />
+        <Route path="/employees/edit/:employeeId" element={<EditEmployee />} />
       </Routes>
     
   );
